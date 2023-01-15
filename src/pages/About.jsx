b7b84a9165da41403506d65458f19ca38ffae751
Yasir <yasirse1223@hotmail.com>
2023-01-15 19:03:30 +0300

    created project with tailwind CSS

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>A React app to search
+      GitHub profiles and see profile details.
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import UserResults from "../components/users/UserResults"
+
+function Home() {
+  return (
+    <>
+      {/* Search Component */}
+      <UserResults />
+    </>
+  )
+}
+
+export default Home
