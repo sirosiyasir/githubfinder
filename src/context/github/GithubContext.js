767ce073f9db1eca821115959c03a62ad058a5a5
Yasir <yasirse1223@hotmail.com>
2023-01-23 17:43:04 +0300

    add user profile

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,10 +9,33 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
+
+  // Get single users
+  const getUser = async (login) => {
+    setLoading()
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    })
+
+    if (response.status === 404) {
+      window.location = "/notfound"
+    } else {
+      const data = await response.json()
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      })
+    }
+  }
 
   // Get search users
   const searchUsers = async (text) => {
@@ -33,6 +56,30 @@ export const GithubProvider = ({ children }) => {
     })
   }
 
+  // Get user repos
+  const getUserRepos = async (login) => {
+    setLoading()
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    })
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+        },
+      }
+    )
+    const data = await response.json()
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    })
+  }
+
   // Clear users from state
   const clearUsers = () => dispatch({ type: "CLEAR_USERS" })
 
@@ -44,8 +91,12 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
+        repos: state.repos,
         searchUsers,
         clearUsers,
+        getUser,
+        getUserRepos,
       }}
     >
       {children}
